e731773f6fbd68789cfc9b79ab27116747faa67d
Konstantin <kuznetsovkm@yandex.ru>
2023-07-22 15:31:00 +0300

    15. Сделан вход выход с сайта, исправлено подключение bootstrap и отображение navbar

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Request
 import models
 from database import engine
 from routers import auth, users, start_page
 from starlette.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from routers.auth import get_current_user
+
+templates = Jinja2Templates(directory='templates/')
 
 app = FastAPI()
 
@@ -13,3 +17,10 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(start_page.router)
+
+@app.get("/")
+async def home(request: Request):
+    user = await get_current_user(request)
+    if user is None:
+        return templates.TemplateResponse('start_page.html', {'request': request})
+    return templates.TemplateResponse('start_page.html', {'request': request, 'user': user})
--- routers/auth.py
+++ routers/auth.py
@@ -128,7 +128,7 @@ async def login(request: Request, db: Session = Depends(get_db)):
     try:
         form = LoginForm(request)
         await form.create_oauth_form()
-        response = RedirectResponse(url='/start_page', status_code=status.HTTP_302_FOUND)
+        response = RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
 
         validate_user_cookie = await login_for_access_token(response=response,
                                                             form_data=form,
