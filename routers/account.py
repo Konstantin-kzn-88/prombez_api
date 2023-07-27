8ab3482f64989e26fc75138e7bdab2aebf05a748
User <kuznetsovkm@yandex.ru>
2023-07-27 18:35:18 +0300

    18. Полная форма редактирования пользователя

--- routers/account.py
+++ routers/account.py
@@ -2,7 +2,6 @@ import sys
 
 sys.path.append('..')
 
-
 from starlette import status
 from starlette.responses import RedirectResponse
 from fastapi import Form, Request, Depends, APIRouter
@@ -23,6 +22,7 @@ router = APIRouter(
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory='templates/')
 
+
 def get_db():
     try:
         db = SessionLocal()
@@ -41,15 +41,24 @@ async def get_account_info(request: Request, db: Session = Depends(get_db)):
 
 
 @router.post('/account_info', response_class=HTMLResponse)
-async def post_account_info(request: Request, email: str = Form(...), db: Session = Depends(get_db)):
-
+async def post_account_info(request: Request,
+                            email: str = Form(...), user_name: str = Form(...),
+                            company_name: str = Form(...), first_name: str = Form(...),
+                            last_name: str = Form(...), phone_number: str = Form(...),
+                            db: Session = Depends(get_db)):
     user = await get_current_user(request)
     if user is None:
         return RedirectResponse(url='/auth', status_code=status.HTTP_302_FOUND)
     user = db.query(models.User).filter(models.User.id == user.get('user_id')).first()
     user.email = email
+    user.user_name = user_name
+    user.company_name = company_name
+    user.first_name = first_name
+    user.last_name = last_name
+    user.phone_number = phone_number
+
     db.add(user)
     db.commit()
 
     msg = 'Данные изменены'
-    return templates.TemplateResponse('account_info.html', {'request': request, 'user': user,  'msg': msg})
+    return templates.TemplateResponse('account_info.html', {'request': request, 'user': user, 'msg': msg})
