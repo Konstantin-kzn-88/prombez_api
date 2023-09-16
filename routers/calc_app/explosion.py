cbb5c377958f16caa259b1a7831f4c32b220c113
User <kuznetsovkm@yandex.ru>
2023-09-16 15:03:30 +0300

    37. Начал оформление методик (добавил методики, сделал вывод в талицу, и график по СП)

--- routers/calc_app/explosion.py
+++ routers/calc_app/explosion.py
@@ -63,7 +63,7 @@ async def post_dev_edit(request: Request,
     y_data = zone_array[1]
 
     plot_div = plot({
-        "data": [Scatter(x=x_data, y=y_data)],
+        "data": [Scatter(x=x_data, y=y_data, line=dict(color='firebrick', width=4))],
         "layout": Layout(title="Взрыв (СП 12.13130-2009)", xaxis=dict(title="X Label"), yaxis=dict(title="У Label"))
     },
         output_type='div', show_link=False, link_text="")
