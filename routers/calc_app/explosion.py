0d318a41876d97ccacc09edbce293b9fc8c2e70b
User <kuznetsovkm@yandex.ru>
2023-09-27 03:42:22 +0300

    37. Начал оформление методик (добавил классифицированные зоны по ТВС)

--- routers/calc_app/explosion.py
+++ routers/calc_app/explosion.py
@@ -89,13 +89,18 @@ async def post_dev_edit(request: Request,
     user = await get_current_user(request)
     if user is None:
         return RedirectResponse(url='/auth', status_code=status.HTTP_302_FOUND)
-
+    # 1. Получим класс для расчета взрыва
     explosion_unit = calc_tvs_explosion.Explosion()
+    # 2. Получим зоны для большой таблицы (не классифицированные)
     zone_array = explosion_unit.explosion_array(int(class_substance), int(view_space), float(mass),
                                                 float(heat_of_combustion), int(sigma), int(energy_level))
     # Преобразование к виду [(r, dP, i, pr, q_vp),...]
     zipped_values = zip(zone_array[0], zone_array[1], zone_array[2], zone_array[3], zone_array[4])
     zipped_list = list(zipped_values)
+    # 3. Получим зоны классифицированные
+    zone_cls_array = explosion_unit.explosion_class_zone(int(class_substance), int(view_space), float(mass),
+                                                float(heat_of_combustion), int(sigma), int(energy_level))
+
 
     plot_div_explosion = plot({
         "data": [Scatter(x=zone_array[0], y=zone_array[1], line=dict(color='#62fb60', width=3))],
@@ -112,6 +117,7 @@ async def post_dev_edit(request: Request,
     return templates.TemplateResponse('calc_app/explosion_tvs_result.html', {'request': request,
                                                                              'user': user,
                                                                              'zone_array': zipped_list,
+                                                                             'zone_cls_array': zone_cls_array,
                                                                              'plot_div_explosion': plot_div_explosion,
                                                                              'plot_div_impuls': plot_div_impuls,
                                                                              'calc_data': (
